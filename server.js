abc15b2a100bc21dc3b6a22a032626f9c849120e
Andres A <acosta.andres.r@gmail.com>
2020-12-03 08:27:50 -0600

    Included app.get with multiple routes

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   // app.use(express.static('client/build'));
   app.use(express.static(path.join(__dirname, 'client/build')));
-  // app.get('/*', function (req, res) {
-  //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  // });
+  app.get(['/', '/portfolio', '/contact'], function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
 // Add routes, both API and view
 app.use("/api", apiRoutes);
