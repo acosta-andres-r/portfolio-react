044ba55d5ac777c41a9c737cacee40f3d3046aab
Andres A <acosta.andres.r@gmail.com>
2020-12-03 00:22:40 -0600

    Deploying

---
+++ client/build/precache-manifest.3d6141b03c7f9353a7d8b1fa982141fd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5fdda4d0547389df8bef39b71fb36db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1556955dc81743bf5ade",
+    "url": "/static/css/2.c9e7500d.chunk.css"
+  },
+  {
+    "revision": "8880659df1fb136b2ce2",
+    "url": "/static/css/main.a65d4cb1.chunk.css"
+  },
+  {
+    "revision": "1556955dc81743bf5ade",
+    "url": "/static/js/2.2f331be9.chunk.js"
+  },
+  {
+    "revision": "de25ba58efe4671c83476e5f3a907c26",
+    "url": "/static/js/2.2f331be9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8880659df1fb136b2ce2",
+    "url": "/static/js/main.c0c82e0c.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "d15da1c178fa8b8ace897cc9849d07ed",
+    "url": "/static/media/AA-logo.d15da1c1.svg"
+  },
+  {
+    "revision": "75c90e146193e4df2e27b1971c0870d7",
+    "url": "/static/media/aesthetica.75c90e14.png"
+  },
+  {
+    "revision": "49ac65f3174449e14fc08061405f1458",
+    "url": "/static/media/employee-directory.49ac65f3.png"
+  },
+  {
+    "revision": "5b6e38b629dc3a8d80edc01a4b923794",
+    "url": "/static/media/general-knowledge.5b6e38b6.png"
+  },
+  {
+    "revision": "041e5cadf9ab76de516d24217cf9a881",
+    "url": "/static/media/note-taker.041e5cad.png"
+  },
+  {
+    "revision": "7ec25fbd285a43605315eb2a68aa4d4b",
+    "url": "/static/media/photo.7ec25fbd.jpg"
+  },
+  {
+    "revision": "f3271dd293b6f640c968744415b31c6a",
+    "url": "/static/media/weather-dashboard.f3271dd2.png"
+  }
+]);
