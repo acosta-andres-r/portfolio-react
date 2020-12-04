a0e088fd5ffc34006587d613ae51744f81d526f6
Andres A <acosta.andres.r@gmail.com>
2020-12-03 21:01:36 -0600

    Rearrange app.use for api

--- server.js
+++ server.js
@@ -8,16 +8,18 @@ const path = require('path');
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Add routes, both API and view
+app.use("/api", apiRoutes);
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   // app.use(express.static('client/build'));
   app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get(['/', '/portfolio', '/contact'], function (req, res) {
+  app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
-// Add routes, both API and view
-app.use("/api", apiRoutes);
 
 // Start the API server
 app.listen(PORT, function() {
