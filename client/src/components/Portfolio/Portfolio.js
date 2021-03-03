28c16e5bfeef401cb2a2b5b97070a9ad1d23abd7
Andres A <acosta.andres.r@gmail.com>
2021-03-03 00:30:04 -0600

    Added dev tools and google book search project

--- client/src/components/Portfolio/Portfolio.js
+++ client/src/components/Portfolio/Portfolio.js
@@ -7,49 +7,63 @@ import aesthetica from "../../assets/Projects/aesthetica.png"
 import noteTaker from "../../assets/Projects/note-taker.png"
 import generalKnowledge from "../../assets/Projects/general-knowledge.png"
 import weatherDashboard from "../../assets/Projects/weather-dashboard.png"
+import googleBookSearch from "../../assets/Projects/google-book-search.png"
 
 const projects = [
     {
         title: "Employee Directory",
-        text: "uses a search box to narrow results and the column to sort in ascending or descending order.",
+        text: "uses a search box to narrow results and each column to sort in ascending or descending order.",
         deployed: "https://acosta-andres-r.github.io/employee-directory/",
-        github: "https://github.com/acosta-andres-r/employee-directory/blob/main/README.md",
-        source: employeedirectory
+        github: "https://github.com/acosta-andres-r/employee-directory",
+        source: employeedirectory,
+        technologies: "React, JSX, CSS, HTML, JavaScript, Random User API"
     },
     {
         title: "Aesthetica",
         text: "is a user friendly and interactive app that bridges fashion and community.",
         deployed: "https://powerful-hamlet-32166.herokuapp.com/",
         github: "https://github.com/acosta-andres-r/aesthetica",
-        source: aesthetica
+        source: aesthetica,
+        technologies: "HTML, CSS, JavaScript, Node.js, Express, MySQL, Handlebars, Pexel API, ORM Sequelize"
+    },
+    {
+        title: "Google Book Search",
+        text: "search for a book and save its information to access later at any time.",
+        deployed: "https://aa-google-book-search.herokuapp.com/",
+        github: "https://github.com/acosta-andres-r/google-books-search",
+        source: googleBookSearch,
+        technologies: "React, JavaScript, Bootstrap, Node.js, Express, MongoDB, Google Books API"
     },
     {
         title: "Note Taker",
         text: "organizes your thoughts and keep track of your tasks by writing, saving, or deleting notes.",
         deployed: "https://stark-mountain-02801.herokuapp.com/",
         github: "https://github.com/acosta-andres-r/note-taker",
-        source: noteTaker
+        source: noteTaker,
+        technologies: "JavaScript, Bootstrap, Node.js, Express, File System (to save data in a JSON file)"
     },
     {
         title: "Weather Dashboard",
-        text: "retrieve weather information from API by city names and store cities in the browser.",
+        text: "retrieves weather information from API by city names and store cities in the browser.",
         deployed: "https://acosta-andres-r.github.io/weather-dashboard/",
         github: "https://github.com/acosta-andres-r/weather-dashboard",
-        source: weatherDashboard
+        source: weatherDashboard,
+        technologies: "HTML, CSS (Flexbox, Transitions), Javascript, jQuery, Open Weather Map API, Local Storage"
     },
     {
         title: "General Knowledge Quiz",
-        text: "Shows multiple choice questions and interactivity with a timer to challenge the user.",
+        text: "shows multiple choice questions and interactivity with a timer to challenge the user.",
         deployed: "https://acosta-andres-r.github.io/general-knowledge-quiz/",
         github: "https://github.com/acosta-andres-r/general-knowledge-quiz",
-        source: generalKnowledge
+        source: generalKnowledge,
+        technologies: "HTML, CSS, Javascript (Animation created with Javasctipt and CSS), Google Fonts, Local Storage"
     }
 ]
 
 const cards = projects.map((project, index) => {
     return (
         <Card style={{
-            width: '18rem',
+            width: '30rem',
             margin: '7px',
             boxShadow: '1px 1px 4px 5px #ccc',
             border: '3px solid #43dde6'
@@ -58,13 +72,14 @@ const cards = projects.map((project, index) => {
                 variant="top"
                 src={project.source}
                 style={{
-                    height: '175px',
+                    height: '300px',
                     border: '1px solid #000000'
                 }}
             />
             <Card.Body>
-                <Card.Title>{project.title}</Card.Title>
-                <Card.Text>{project.text}</Card.Text>
+                <Card.Title><strong>{project.title}</strong></Card.Title>
+                <Card.Text>{project.text}</Card.Text> 
+                <Card.Text><u>Tools</u>: {project.technologies}</Card.Text> 
                 <Card.Link
                     href={project.deployed}
                     target="_blank"
